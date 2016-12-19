bc40a83709a477aba759754f9d14ceff20eafa5f
mwatha <mwathabwanali@gmail.com>
2016-12-19 13:52:11 +0200

    encr: all demographics

--- app/models/openmrs.rb
+++ app/models/openmrs.rb
@@ -29,7 +29,7 @@ EOF
 
         unless row['given_name'].blank?
           ActiveRecord::Base.connection.execute <<EOF
-          UPDATE person_name SET given_name = '#{row['given_name'].encrypt}'
+          UPDATE person_name SET given_name = "#{row['given_name'].encrypt}"
           WHERE person_name_id = #{person_name_id};
 EOF
 
@@ -37,7 +37,7 @@ EOF
 
         unless row['middle_name'].blank?
           ActiveRecord::Base.connection.execute <<EOF
-          UPDATE person_name SET middle_name = '#{row['middle_name'].encrypt}'
+          UPDATE person_name SET middle_name = "#{row['middle_name'].encrypt}"
           WHERE person_name_id = #{person_name_id};
 EOF
 
@@ -45,7 +45,7 @@ EOF
 
         unless row['family_name'].blank?
           ActiveRecord::Base.connection.execute <<EOF
-          UPDATE person_name SET family_name = '#{row['family_name'].encrypt}'
+          UPDATE person_name SET family_name = "#{row['family_name'].encrypt}"
           WHERE person_name_id = #{person_name_id};
 EOF
 
@@ -55,9 +55,103 @@ EOF
       end
       ################################## person_name ############################################
 
+      
+      ################################## person_address ############################################
+      ActiveRecord::Base.connection.execute <<EOF
+        ALTER TABLE person_address
+        CHANGE COLUMN `address1` `address1` VARCHAR(150) NULL DEFAULT NULL ,
+        CHANGE COLUMN `address2` `address2` VARCHAR(150) NULL DEFAULT NULL ,
+        CHANGE COLUMN `city_village` `city_village` VARCHAR(150) NULL DEFAULT NULL ;
+EOF
+
+      addresses = ActiveRecord::Base.connection.select_all <<EOF
+      SELECT * FROM person_address;
+EOF
+
+      (addresses || []).each do |row|
+        person_address_id = row['person_address_id'].to_i;
+
+        unless row['address1'].blank?
+          ActiveRecord::Base.connection.execute <<EOF
+          UPDATE person_address SET address1 = "#{row['address1'].encrypt}"
+          WHERE person_address_id = #{person_address_id};
+EOF
+
+        end
+
+        unless row['address2'].blank?
+          ActiveRecord::Base.connection.execute <<EOF
+          UPDATE person_address SET address2 = "#{row['address2'].encrypt}"
+          WHERE person_address_id = #{person_address_id};
+EOF
+
+        end
+
+        unless row['city_village'].blank?
+          ActiveRecord::Base.connection.execute <<EOF
+          UPDATE person_address SET city_village = "#{row['city_village'].encrypt}"
+          WHERE person_address_id = #{person_address_id};
+EOF
+
+        end
+
+        puts ".... Encrypted person_address: #{person_address_id}"
+      end
+      ################################## person_address ############################################
 
 
 
+      ################################## patient_identifier ############################################
+      ActiveRecord::Base.connection.execute <<EOF
+        ALTER TABLE patient_identifier
+        CHANGE COLUMN `identifier` `identifier` VARCHAR(150) NOT NULL DEFAULT '' ;
+
+EOF
+
+      patient_identifiers = ActiveRecord::Base.connection.select_all <<EOF
+      SELECT * FROM patient_identifier;
+EOF
+
+      (patient_identifiers || []).each do |row|
+        patient_identifier_id = row['patient_identifier_id'].to_i;
+
+        unless row['identifier'].blank?
+          ActiveRecord::Base.connection.execute <<EOF
+          UPDATE person_address SET identifier = "#{row['identifier'].encrypt}"
+          WHERE patient_identifier_id = #{patient_identifier_id};
+EOF
+
+        end
+
+        puts "Encrypted patient_identifier_id: #{patient_identifier_id}"
+      end
+      ################################## patient_identifier ############################################
+
+      ################################## person_attribute ############################################
+      ActiveRecord::Base.connection.execute <<EOF
+        ALTER TABLE person_attribute
+        CHANGE COLUMN `value` `identifier` VARCHAR(150) NOT NULL DEFAULT '' ;
+
+EOF
+
+      person_attributes = ActiveRecord::Base.connection.select_all <<EOF
+      SELECT * FROM person_attribute;
+EOF
+
+      (person_attributes || []).each do |row|
+        person_attribute_id = row['person_attribute_id'].to_i;
+
+        unless row['value'].blank?
+          ActiveRecord::Base.connection.execute <<EOF
+          UPDATE person_attribute SET value = "#{row['value'].encrypt}"
+          WHERE person_attribute_id = #{person_attribute_id};
+EOF
+
+        end
+
+        puts "Encrypted person_attribute_id: #{person_attribute_id}"
+      end
+      ################################## person_attribute ############################################
 
     end
 
@@ -91,7 +185,7 @@ EOF
 
         unless row['given_name'].blank?
           ActiveRecord::Base.connection.execute <<EOF
-          UPDATE person_name SET given_name = '#{row['given_name'].decrypt}'
+          UPDATE person_name SET given_name = "#{row['given_name'].decrypt}"
           WHERE person_name_id = #{person_name_id};
 EOF
 
@@ -99,7 +193,7 @@ EOF
 
         unless row['middle_name'].blank?
           ActiveRecord::Base.connection.execute <<EOF
-          UPDATE person_name SET middle_name = '#{row['middle_name'].decrypt}'
+          UPDATE person_name SET middle_name = "#{row['middle_name'].decrypt}"
           WHERE person_name_id = #{person_name_id};
 EOF
 
@@ -107,7 +201,7 @@ EOF
 
         unless row['family_name'].blank?
           ActiveRecord::Base.connection.execute <<EOF
-          UPDATE person_name SET family_name = '#{row['family_name'].decrypt}'
+          UPDATE person_name SET family_name = "#{row['family_name'].decrypt}"
           WHERE person_name_id = #{person_name_id};
 EOF
 
@@ -117,15 +211,109 @@ EOF
       end
       ################################## person_name ############################################
 
+      ################################## person_address ############################################
+      addresses = ActiveRecord::Base.connection.select_all <<EOF
+      SELECT * FROM person_address;
+EOF
+
+      (addresses || []).each do |row|
+        person_address_id = row['person_address_id'].to_i;
+
+        unless row['address1'].blank?
+          ActiveRecord::Base.connection.execute <<EOF
+          UPDATE person_address SET address1 = "#{row['address1'].decrypt}"
+          WHERE person_address_id = #{person_address_id};
+EOF
+
+        end
+
+        unless row['address2'].blank?
+          ActiveRecord::Base.connection.execute <<EOF
+          UPDATE person_address SET address2 = "#{row['address2'].decrypt}"
+          WHERE person_address_id = #{person_address_id};
+EOF
+
+        end
+
+        unless row['city_village'].blank?
+          ActiveRecord::Base.connection.execute <<EOF
+          UPDATE person_address SET city_village = "#{row['city_village'].decrypt}"
+          WHERE person_address_id = #{person_address_id};
+EOF
+
+        end
+
+        puts ".... Decrypted person_address: #{person_address_id}"
+      end
+        
+      ActiveRecord::Base.connection.execute <<EOF
+        ALTER TABLE person_address
+        CHANGE COLUMN `address1` `address1` VARCHAR(50) NULL DEFAULT NULL ,
+        CHANGE COLUMN `address2` `address2` VARCHAR(50) NULL DEFAULT NULL ,
+        CHANGE COLUMN `city_village` `city_village` VARCHAR(50) NULL DEFAULT NULL ;
+EOF
+
+      ################################## person_address ############################################
+
+      ################################## patient_identifier ############################################
+      patient_identifiers = ActiveRecord::Base.connection.select_all <<EOF
+      SELECT * FROM patient_identifier;
+EOF
+
+      (patient_identifiers || []).each do |row|
+        patient_identifier_id = row['patient_identifier_id'].to_i;
+
+        unless row['identifier'].blank?
+          ActiveRecord::Base.connection.execute <<EOF
+          UPDATE person_address SET identifier = "#{row['identifier'].decrypt}"
+          WHERE patient_identifier_id = #{patient_identifier_id};
+EOF
+
+        end
+
+        puts "Encrypted patient_identifier_id: #{patient_identifier_id}"
+      end
+      
+      ActiveRecord::Base.connection.execute <<EOF
+        ALTER TABLE patient_identifier
+        CHANGE COLUMN `identifier` `identifier` VARCHAR(50) NOT NULL DEFAULT '' ;
+
+EOF
+
+      ################################## patient_identifier ############################################
+
+      ################################## person_attribute ############################################
+      person_attributes = ActiveRecord::Base.connection.select_all <<EOF
+      SELECT * FROM person_attribute;
+EOF
+
+      (person_attributes || []).each do |row|
+        person_attribute_id = row['person_attribute_id'].to_i;
+
+        unless row['value'].blank?
+          ActiveRecord::Base.connection.execute <<EOF
+          UPDATE person_attribute SET value = "#{row['value'].decrypt}"
+          WHERE person_attribute_id = #{person_attribute_id};
+EOF
+
+        end
+
+        puts "Decrypted person_attribute_id: #{person_attribute_id}"
+      end
+      
+      ActiveRecord::Base.connection.execute <<EOF
+        ALTER TABLE person_attribute
+        CHANGE COLUMN `value` `identifier` VARCHAR(50) NOT NULL DEFAULT '' ;
+
+EOF
+
+      ################################## person_attribute ############################################
+
+    end #rescue nil
 
 
 
-
-    end
-
-
-
-
+    puts "Decrypted .... "
   end
 
 end
